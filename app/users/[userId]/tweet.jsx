d5f7bed7776d904752db8833d430fda95f6f9d5b
Hamed Bahram <64147793+HamedBahram@users.noreply.github.com>
2022-11-19 13:33:31 -0500

    Update tweet.jsx

--- app/users/[userId]/tweet.jsx
+++ app/users/[userId]/tweet.jsx
@@ -1,17 +1,47 @@
 'use client'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { HeartIcon } from '@heroicons/react/24/outline'
+import { HeartIcon as HartIconFilled } from '@heroicons/react/24/solid'
 
 const Tweet = ({ tweet }) => {
-  const [likes, setLikes] = useState(1)
+  const [likes, setLikes] = useState(tweet?.likes)
+  const [liked, setLiked] = useState(null)
+  useEffect(() => {
+    if (liked === null) return
+
+    const endpoint = liked
+      ? `/api/tweets/${tweet.id}/like`
+      : `/api/tweets/${tweet.id}/unlike`
+
+    fetch(endpoint, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(({ tweet }) => {
+        setLikes(tweet?.likes)
+      })
+  }, [liked])
+
+  const handleClick = () => setLiked(liked => !liked)
 
   return (
-    <li className='my-4 rounded bg-stone-200 p-4 text-stone-600'>
+    <li className='my-3 rounded bg-stone-200 p-4 text-stone-500'>
       <div className='flex justify-between'>
         <p>{tweet.body}</p>
-        <button>
-          <span>{likes}</span>
-        </button>
+        <div className='flex items-center gap-2'>
+          <button onClick={handleClick}>
+            {liked ? (
+              <HartIconFilled className='w-7 h-7 text-red-500' />
+            ) : (
+              <HeartIcon className='w-7 h-7 text-red-500' />
+            )}
+          </button>
+          <span className='text-sm'>{likes}</span>
+        </div>
       </div>
     </li>
   )
