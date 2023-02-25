630e525d0514f4161ee1a72b64d02812e667cfa5
HamedBahram <64147793+HamedBahram@users.noreply.github.com>
2023-02-25 12:58:16 -0500

    Add google font via next/font package

--- app/(site)/layout.jsx
+++ app/(site)/layout.jsx
@@ -3,9 +3,22 @@ import Footer from './footer'
 import Header from './header'
 import Provider from './provider'
 
+import { Nunito_Sans } from 'next/font/google'
+
+// Fonts
+const nunito = Nunito_Sans({
+  subsets: ['latin'],
+  weight: ['300', '400', '600', '700', '800', '900'],
+  style: ['normal', 'italic'],
+  variable: '--font-nunito'
+})
+
 export default function RootLayout({ children }) {
   return (
-    <html lang='en' className='h-full scroll-smooth antialiased'>
+    <html
+      lang='en'
+      className={`${nunito.className} h-full scroll-smooth antialiased`}
+    >
       <head />
       <body className='flex h-full flex-col'>
         <Provider>
